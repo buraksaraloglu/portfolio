c6e0a6b41b7c5ace060208e514dca8b249839f0c
Burak Saraloglu <buraksaraloglu1@gmail.com>
2020-12-24 17:06:02 +0300

    Https fix

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import {
 import './App.scss';
 import { HomeContainer } from './containers/home';
 import { ProjectContainer } from './containers/project';
-import firebase from 'firebase/app';
-import 'firebase/analytics';
+// import firebase from 'firebase/app';
+// import 'firebase/analytics';
 
 export const App = () => {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyCXgSslyJrYg_BchJ9vuXeUVhlpJFnQRnc',
-    authDomain: 'buraksaraloglu.com',
-    projectId: 'portfolio-e4ce2',
-    storageBucket: 'portfolio-e4ce2.appspot.com',
-    messagingSenderId: '974870440786',
-    appId: '1:974870440786:web:91ee073c0a77ee1463288b',
-    measurementId: 'G-6SPVG1BS56',
-  };
+  // const firebaseConfig = {
+  //   apiKey: 'AIzaSyCXgSslyJrYg_BchJ9vuXeUVhlpJFnQRnc',
+  //   authDomain: 'buraksaraloglu.com',
+  //   projectId: 'portfolio-e4ce2',
+  //   storageBucket: 'portfolio-e4ce2.appspot.com',
+  //   messagingSenderId: '974870440786',
+  //   appId: '1:974870440786:web:91ee073c0a77ee1463288b',
+  //   measurementId: 'G-6SPVG1BS56',
+  // };
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  firebaseApp.analytics();
+  // const firebaseApp = firebase.initializeApp(firebaseConfig);
+  // firebaseApp.analytics();
 
   return (
     <Router>
