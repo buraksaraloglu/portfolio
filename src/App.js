caea6a320e8db77183c9ceba82b0e0f9a783da23
Burak Saraloglu <buraksaraloglu1@gmail.com>
2020-12-24 16:54:06 +0300

    Https error fix

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ import {
 import './App.scss';
 import { HomeContainer } from './containers/home';
 import { ProjectContainer } from './containers/project';
-import Firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/analytics';
-import reportWebVitals from './reportWebVitals';
 
 export const App = () => {
   const firebaseConfig = {
     apiKey: 'AIzaSyCXgSslyJrYg_BchJ9vuXeUVhlpJFnQRnc',
-    authDomain: 'portfolio-e4ce2.firebaseapp.com',
+    authDomain: 'buraksaraloglu.com',
     projectId: 'portfolio-e4ce2',
     storageBucket: 'portfolio-e4ce2.appspot.com',
     messagingSenderId: '974870440786',
@@ -23,19 +22,8 @@ export const App = () => {
     measurementId: 'G-6SPVG1BS56',
   };
 
-  const firebase = Firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-  function sendToAnalytics({ id, name, value }) {
-    window.gtag('send', 'event', {
-      eventCategory: 'Web Vitals',
-      eventAction: name,
-      eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-      eventLabel: id, // id unique to current page load
-      nonInteraction: true, // avoids affecting bounce rate
-    });
-  }
-  reportWebVitals(sendToAnalytics);
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  firebaseApp.analytics();
 
   return (
     <Router>
